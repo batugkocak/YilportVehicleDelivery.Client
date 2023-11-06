e05edfbd6a659fd5b343ca5034f702a858f27d3d
Batug Kocak <batugkocak@gmail.com>
2023-11-06 13:48:18 +0300

    feat: DriverView and functionalities are done.  Delete will be added

--- src/views/DriversView.vue
+++ src/views/DriversView.vue
@@ -1,13 +1,43 @@
 <template>
-  <drivers-table></drivers-table>
+  <drivers-table
+    @toggle-add="openAddDialog"
+    @open-snackbar="openSnackbar"
+    ref="driverTable"
+  />
+  <add-driver-dialog
+    v-model="addDialog"
+    @add-task="updateTable"
+    @open-snackbar="openSnackbar"
+  />
 </template>
 
 <script>
-import DriversTable from "@/components/others/DriversTable";
+import DriversTable from "@/components/drivers/DriversTable";
+import AddDriverDialog from "@/components/drivers/AddDriverDialog";
 
 export default {
   components: {
     DriversTable,
+    AddDriverDialog,
+  },
+  data() {
+    return {
+      addDialog: false,
+    };
+  },
+  methods: {
+    openAddDialog() {
+      this.addDialog = true;
+    },
+    updateTable() {
+      this.$refs.driverTable.fetchDrivers();
+    },
+    openSnackbar(isSuccess, message) {
+      this.$root.snackBar.show({
+        isSuccess: isSuccess,
+        message: message,
+      });
+    },
   },
 };
 </script>
