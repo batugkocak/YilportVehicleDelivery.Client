c5fda46a4beb53f56a867ed84c8131775bf79515
Batug Kocak <batugkocak@gmail.com>
2023-11-14 10:27:10 +0300

    added new VehicleType

--- src/common/constants/vehicleType.js
+++ src/common/constants/vehicleType.js
@@ -1,37 +1,45 @@
-const vehicleTypes= [
-// {
-//   name: "None",
-//   id:0
-// },
-{
-  name: "Otomobil",
-  id:1
-},
-{
-  name: "Ticari",
-  id:2
-},
-{
-  name: "Transit",
-  id:3
-},
-{
-  name: "Ambulans",
-  id:4
-},
-{
-  name: "Traktör",
-  id:5
-},
-{
-  name: "İtfaiye",
-  id:6
-},
-{
-  name: "Kamyonet",
-  id:7
-},
-]
+const vehicleTypes = [
+  // {
+  //   name: "None",
+  //   id:0
+  // },
+  {
+    name: "Otomobil",
+    id: 1,
+  },
+  {
+    name: "Ticari",
+    id: 2,
+  },
+  {
+    name: "Transit",
+    id: 3,
+  },
+  {
+    name: "Ambulans",
+    id: 4,
+  },
+  {
+    name: "Traktör",
+    id: 5,
+  },
+  {
+    name: "İtfaiye",
+    id: 6,
+  },
+  {
+    name: "Kamyonet",
+    id: 7,
+  },
+  {
+    name: "Arazöz",
+    id: 8,
+  },
+  {
+    name: "Diğer",
+    id: 9,
+  },
+];
 export default {
- vehicleTypes : vehicleTypes
-}
+  vehicleTypes: vehicleTypes,
+};
